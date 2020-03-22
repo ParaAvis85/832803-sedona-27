b503ac79d9a4d616d8695eee5ee4314c8d8916be
ParaAvis <Grynza_let@mail.ru>
2020-03-22 14:16:19 +0300

    Черновой вариант js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+var button = document.querySelector(".popup-btn");
+var form = document.querySelector(".modal-form");
+
+var adult = form.querySelector("[name=adult]");
+var child = form.querySelector("[name=child]");
+var arrival = form.querySelector("[name=arrival]");
+var departure = form.querySelector("[name=departure]");
+
+var isStorageSupport = true;
+var storageAdult = "";
+var storageChild = "";
+
+try {
+ storageAdult = localStorage.getitem("adult");
+ storageChild = localStorage.getItem("child");
+} 
+catch (err) {
+ isStorageSupport = false;
+}
+
+button.addEventListener("click", function(evt) {
+ evt.preventDefault();
+ form.classList.toggle("popup-show");
+ form.classList.remove("popup-error");
+});
+
+form.addEventListener("submit", function (evt) {
+ if (!arrival.value || !departure.value || !adult.value || !child.value) {
+     evt.preventDefault();
+     form.classList.remove("popup-error");
+     form.offsetWidth = form.offsetWidth;
+      form.classList.add("popup-error");
+     if (!departure.value) {
+         departure.focus();
+         if (!entry.value);
+             entry.focus();
+         }   
+     } else if (!adult.value) {
+         adult.focus();
+     } else child.focus();
+ });
+
+window.addEventListenert("keydown", function(evt) {
+     if (evt.keyCode === 27) {
+    
+         if (form.classList.contains ("popup-show")) {
+             form.classlist.add ("popup-show");
+             form.classList.remove ("popup-error");
+            }
+        }
+    });
+
+
+
